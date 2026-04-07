76382a00ae9ed2e64d58cf5f9d629f3b3796452a
kupriyanovin@mer.ci.nsu.ru <kupriyanovin@mer.ci.nsu.ru>
2026-04-07 19:02:49 +0700

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Lab1_Kupriyanov_Ilya
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
